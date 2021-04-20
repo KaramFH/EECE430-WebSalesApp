aa39d087da0174241c7fb4aabcb355359ae0c7fa
KaramFH <49188527+KaramFH@users.noreply.github.com>
2021-04-21 00:54:46 +0300

    added models in models.py and fixed the html code

--- store/models.py
+++ store/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Products(models.Model):
+	title = models.CharField(max_length = 100)
+	description = models.TextField()
+	price = models.IntegerField()
+	amount = models.IntegerField()
+	picture = models.ImageField()
+	item_number = models.TextField()
+	barcode = models.TextField()
+
+
+class Customer(models.Model):
+	firstname = models.TextField()
+	lastname = models.TextField()
+	dateofbirth = models.DateField()
+	phonenumber =  models.TextField()
+	email = models.EmailField()
+	province =  models.TextField()
+	city =  models.TextField()
+	addresss =  models.TextField()
+
+
+
